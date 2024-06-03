0a369be0ce98532b0720775600da20f851ef4c9f
Vanlinh210704 <linhlinh2159@gmail.com>
2024-06-03 21:51:04 +0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const path = require('path');
 var view_cesium_router = require("./routers/view_cesium_router");
 var view_panorama_router = require("./routers/view_panorama_router");
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server đang chạy tại http://localhost:${port}`);
+});
 
 app.set("view engine","ejs");
 app.set("views","./views");
