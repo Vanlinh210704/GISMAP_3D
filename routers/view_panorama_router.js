324f357c3baec86ef6d940f47b8bacb2137148fd
Vanlinh210704 <linhlinh2159@gmail.com>
2024-06-04 23:05:26 +0700

    Vanlinh update

--- routers/view_panorama_router.js
+++ routers/view_panorama_router.js
@@ -2,10 +2,14 @@ var express = require("express");
 const pool = require("../db/connect").pool; 
 var view_panorama_router = express.Router();
 
-view_panorama_router.get("/", function(req, res) {
-    var query_li = "SELECT image_link FROM panorama_image_catalog WHERE building_id = 2";
+view_panorama_router.get("/:id", function(req, res) {
+    // Lấy id từ URL
+    const id = req.params.id;
     
-    pool.query(query_li, (err, result) => {
+    // Sử dụng id trong câu lệnh SQL
+    var query_li = "SELECT image_link FROM panorama_image_catalog WHERE building_id = $1";
+    
+    pool.query(query_li, [id], (err, result) => {
         if (err) {
             console.error('Lỗi khi thực thi truy vấn:', err.stack);
             res.status(500).send('Lỗi máy chủ nội bộ');
