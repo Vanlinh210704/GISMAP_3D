fd15f765925c1f47a16019e14a30c2985b5d94e2
Vanlinh210704 <linhlinh2159@gmail.com>
2024-06-06 18:03:59 +0700

    update view panorama

--- routers/view_panorama_router.js
+++ routers/view_panorama_router.js
@@ -7,16 +7,17 @@ view_panorama_router.get("/:id", function(req, res) {
     const id = req.params.id;
     
     // Sử dụng id trong câu lệnh SQL
-    var query_li = "SELECT image_link FROM panorama_image_catalog WHERE building_id = $1";
-    
+    var query_li = "SELECT * FROM panorama_image_catalog AS pic INNER JOIN building_data AS bd ON pic.building_id = bd.building_id WHERE bd.building_id = $1"; 
+
     pool.query(query_li, [id], (err, result) => {
         if (err) {
             console.error('Lỗi khi thực thi truy vấn:', err.stack);
             res.status(500).send('Lỗi máy chủ nội bộ');
             return;
         }
+        const data_bd = result.rows;
         const imagelinks = result.rows.map(row => row.image_link);
-        res.render('viewpanorama', { imagelinks }); 
+        res.render('viewpanorama', { data_bd , imagelinks}); 
     });
 });
 
